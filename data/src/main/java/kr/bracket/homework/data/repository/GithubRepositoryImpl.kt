a8102532d67e5cffe24c077107984a1683b6cf0d
Marihuana <Bracket0723@gmail.com>
2021-11-12 20:31:34 +0900

    > #5 데이터 작성 Repository 작성

--- data/src/main/java/kr/bracket/homework/data/repository/GithubRepositoryImpl.kt
+++ data/src/main/java/kr/bracket/homework/data/repository/GithubRepositoryImpl.kt
@@ -1,6 +1,21 @@
 package kr.bracket.homework.data.repository
 
+import io.reactivex.rxjava3.core.Single
+import kr.bracket.homework.data.datasource.GithubService
+import kr.bracket.homework.data.mapper.RepoMapper
+import kr.bracket.homework.domain.model.RepoVO
 import kr.bracket.homework.domain.repository.GithubRepository
 
-class GithubRepositoryImpl : GithubRepository {
+class GithubRepositoryImpl (private val service : GithubService): GithubRepository {
+    private val pageSize = 30
+
+    override fun searchRepositories(query: String): Single<List<RepoVO>> {
+        val fixed = "$query$QUERY_SUFFIX"
+
+        return service.getRepositories(fixed, 1, pageSize).map(RepoMapper::transform)
+    }
+
+    companion object {
+        const val QUERY_SUFFIX = " in:name"
+    }
 }
